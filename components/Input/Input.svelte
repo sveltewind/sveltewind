fe1fe92fffc898a220f5dd8b6aa3c52782da0c51
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-03 10:36:54 -0500

    add Input component

---
+++ components/Input/Input.svelte
@@ -0,0 +1,27 @@
+<script>
+  // imports
+  import { twMerge } from 'tailwind-merge';
+  import { setType } from '../../actions';
+  import { theme } from '../../stores';
+
+  // handlers
+
+  // utilities
+
+  // props (internal)
+  const defaultClasses = '';
+
+  // props (external)
+  export let type = 'text';
+  export let value = '';
+
+  // props (dynamic)
+  $: classes = twMerge(defaultClasses, $theme.input, $$props.class);
+</script>
+
+<input
+  {...$$props}
+  bind:value
+  class={classes}
+  use:setType={type}
+/>
--- components/index.js
+++ components/index.js
@@ -9,5 +9,6 @@ export { default as H4 } from "./H4/index.js";
 export { default as H5 } from "./H5/index.js";
 export { default as H6 } from "./H6/index.js";
 export { default as Icon } from "./Icon/index.js";
+export { default as Input } from "./Input/index.js";
 
 export { default as Legend } from "./Legend/index.js";
