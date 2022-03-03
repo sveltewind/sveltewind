5de43221ddf96101a607df828566d87def684d43
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-03 10:29:12 -0500

    add H4 component

---
+++ components/H4/H4.svelte
@@ -0,0 +1,23 @@
+<script>
+  // imports
+  import { twMerge } from 'tailwind-merge';
+  import { theme } from '../../stores';
+
+  // handlers
+
+  // utilities
+
+  // props (internal)
+  const defaultClasses = '';
+
+  // props (external)
+
+  // props (dynamic)
+  $: classes = twMerge(defaultClasses, $theme.h4, $$props.class);
+</script>
+
+<h4
+  class={classes}
+>
+  <slot />
+</h4>
