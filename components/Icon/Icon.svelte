ceb2242de1e6c02319a568b69841e9deec956d98
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-03 10:33:36 -0500

    add Icon component

---
+++ components/Icon/Icon.svelte
@@ -0,0 +1,23 @@
+<script>
+  // imports
+  import { Icon } from 'svelte-hero-icons';
+  import { twMerge } from 'tailwind-merge';
+  import { theme } from '../../stores';
+
+  // handlers
+
+  // utilities
+
+  // props (internal)
+  const defaultClasses = '';
+
+  // props (external)
+  export let src;
+
+  // props (dynamic)
+  $: classes = twMerge(defaultClasses, $theme.icon, $$props.class);
+</script>
+
+{#if src !== undefined}
+  <Icon {src} class={classes} />
+{/if}
--- components/index.js
+++ components/index.js
@@ -8,5 +8,6 @@ export { default as H3 } from "./H3/index.js";
 export { default as H4 } from "./H4/index.js";
 export { default as H5 } from "./H5/index.js";
 export { default as H6 } from "./H6/index.js";
+export { default as Icon } from "./Icon/index.js";
 
 export { default as Legend } from "./Legend/index.js";
