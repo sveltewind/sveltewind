49738f0e8efe60b0147a5e7a978fe44c5cd4198a
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-03 08:07:21 -0500

    add Button component

---
+++ components/Button/Button.svelte
@@ -0,0 +1,23 @@
+<script>
+  // imports
+  import { twMerge } from 'tailwind-merge';
+  import { setType } from '../../actions';
+  import { theme } from '../../stores';
+
+  // props (internal)
+  const defaultClasses = '';
+
+  // props (external)
+  export let type = 'button';
+
+  // props (dynamic)
+  $: classes = twMerge(defaultClasses, $theme.button, $$props.class);
+</script>
+
+<button 
+  class={classes}
+  on:click
+  use:setType={type}
+>
+  <slot />
+</button>
