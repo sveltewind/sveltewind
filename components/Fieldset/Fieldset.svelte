6e4fa968970c2478717fc09ec848bd0d4a81995f
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-03 10:26:03 -0500

    add Fieldset component

---
+++ components/Fieldset/Fieldset.svelte
@@ -0,0 +1,29 @@
+<script>
+  // imports
+  import { twMerge } from 'tailwind-merge';
+  import { theme } from '../../stores';
+  import Legend from '../../components/Legend/index.js'
+
+  // handlers
+
+  // utilities
+
+  // props (internal)
+  const defaultClasses = '';
+
+  // props (external)
+  export let legend = '';
+  export let showLegend = true;
+
+  // props (dynamic)
+  $: classes = twMerge(defaultClasses, $theme.fieldset, $$props.class);
+</script>
+
+<fieldset
+  class={classes}
+>
+  {#if showLegend}
+    <Legend>{legend}</Legend>
+  {/if}
+  <slot />
+</fieldset>
