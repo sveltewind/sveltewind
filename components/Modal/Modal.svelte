8ef3f103994aa3d2ef992556d9de123b5059d03b
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-03 16:20:22 -0500

    add common styling components

--- components/Modal/Modal.svelte
+++ components/Modal/Modal.svelte
@@ -1,7 +1,8 @@
 <script>
   // imports
   import { twMerge } from 'tailwind-merge';
-  import Overlay from '../../components/Overlay/index.js';
+  import { X } from 'svelte-hero-icons';
+  import { Button, Card, H6, Icon, Overlay, P } from '../../components/index.js';
   import { theme } from '../../stores';
 
   // handlers
@@ -12,8 +13,12 @@
   const defaultClasses = '';
 
   // props (external)
+  export let body;
+  export let showBodyClose = false;
   export let showOverlay = true;
+  export let showTitleClose = false;
   export let show = false;
+  export let title;
 
   // props (dynamic)
   $: classes = twMerge(defaultClasses, show ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none', $theme.modal, $$props.class);
@@ -24,6 +29,31 @@
 >
   {#if showOverlay}
     <Overlay on:click={() => show = !show} class="cursor-pointer" />
+  {/if}
+  {#if showBodyClose !== false || showTitleClose !== false || title !== undefined}
+    <Card class="relative space-y-[2rem] max-w-lg max-h-screen">
+      {#if showTitleClose !== false || title !== undefined}
+        <div class="flex items-center {showTitleClose === true && title !== undefined ? 'justify-between space-x-[2rem]' : showTitleClose === true ? 'justify-end' : 'justify-start'}">
+          {#if title !== undefined}
+            <H6>{title}</H6>
+          {/if}
+          {#if showTitleClose !== false}
+          <Button
+            class="bg-transparent hover:bg-black/5 focus:bg-black/5 focus:ring-black/30 text-black px-[.5rem]"
+            on:click={() => (show = !show)}><Icon src={X} /></Button
+          >
+          {/if}
+        </div>
+      {/if}
+      {#if body !== undefined}
+        <P class="overflow-auto">{body}</P>
+      {/if}
+      {#if showBodyClose !== false}
+        <div class="flex justify-end">
+          <Button on:click={() => (show = !show)}>Close</Button>
+        </div>
+      {/if}
+    </Card>
   {/if}
   <slot />
 </div>
