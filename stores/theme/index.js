d3428e746660e93e46834e4c3a6f809544a3d8c6
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-03 09:17:40 -0500

    refactor theme store for custom methods

--- stores/theme/index.js
+++ stores/theme/index.js
@@ -1,5 +1,22 @@
 // _imports
 import { writable } from 'svelte/store';
+import { twMerge } from 'tailwind-merge';
 import { dream } from '../../themes/index.js';
 
-export default writable(dream);
+const init = theme => {
+  // destructure methods
+  const { subscribe, update } = writable(theme);
+
+  return {
+    set: {
+      component: ({ component, classes }) =>
+        update(obj => {
+          obj[component] = twMerge(obj[component], classes);
+          return obj;
+        }),
+    },
+    subscribe,
+  };
+};
+
+export default init(dream);
