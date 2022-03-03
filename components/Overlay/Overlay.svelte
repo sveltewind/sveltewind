6a7cce281b1504a6ce361c47fc98c0361ee35ba4
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-03 12:50:33 -0500

    add Overlay component

---
+++ components/Overlay/Overlay.svelte
@@ -0,0 +1,24 @@
+<script>
+  // imports
+  import { twMerge } from 'tailwind-merge';
+  import { theme } from '../../stores';
+
+  // handlers
+
+  // utilities
+
+  // props (internal)
+  const defaultClasses = '';
+
+  // props (external)
+
+  // props (dynamic)
+  $: classes = twMerge(defaultClasses, $theme.overlay, $$props.class);
+</script>
+
+<div
+  class={classes}
+  on:click
+>
+  <slot />
+</div>
--- components/index.js
+++ components/index.js
@@ -14,3 +14,4 @@ export { default as Legend } from "./Legend/index.js";
 export { default as Main } from "./Main/index.js";
 export { default as Modal } from "./Modal/index.js";
 export { default as Nav } from "./Nav/index.js";
+export { default as Overlay } from "./Overlay/index.js";
