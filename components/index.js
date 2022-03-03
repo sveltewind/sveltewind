d1e05aefa3748594375c72b6ce72cc37996a32ef
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-03 10:28:38 -0500

    add H3 component

--- components/index.js
+++ components/index.js
@@ -4,5 +4,6 @@ export { default as Fieldset } from "./Fieldset/index.js";
 export { default as Form } from "./Form/index.js";
 export { default as H1 } from "./H1/index.js";
 export { default as H2 } from "./H2/index.js";
+export { default as H3 } from "./H3/index.js";
 
 export { default as Legend } from "./Legend/index.js";
