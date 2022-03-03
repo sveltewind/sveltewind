dcf4621a33b10ca51164308020486df8614213b1
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-03 10:26:43 -0500

    add Form component

---
+++ components/Form/Form.svelte
@@ -0,0 +1,24 @@
+<script>
+  // imports
+  import { twMerge } from 'tailwind-merge';
+  import { theme } from '../../stores';
+
+  // handlers
+
+  // utilities
+
+  // props (internal)
+  const defaultClasses = '';
+
+  // props (external)
+
+  // props (dynamic)
+  $: classes = twMerge(defaultClasses, $theme.form, $$props.class);
+</script>
+
+<form
+  class={classes}
+  on:submit
+>
+  <slot />
+</form>
--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './Button/index.js';
 export { default as Card } from "./Card/index.js";
 export { default as Fieldset } from "./Fieldset/index.js";
+export { default as Form } from "./Form/index.js";
 
 export { default as Legend } from "./Legend/index.js";
