b6aa214f3fac085f354adacabe7998f0e43c8f8e
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-03 14:57:23 -0500

    add Th component

---
+++ components/Th/Th.svelte
@@ -0,0 +1,23 @@
+<script>
+  // imports
+  import { twMerge } from 'tailwind-merge';
+  import { theme } from '../../stores';
+
+  // handlers
+
+  // utilities
+
+  // props (internal)
+  const defaultClasses = '';
+
+  // props (external)
+
+  // props (dynamic)
+  $: classes = twMerge(defaultClasses, $theme.th, $$props.class);
+</script>
+
+<th
+  class={classes}
+>
+  <slot />
+</th>
