c3bd234bb3d12b6c955a3c57e5c53278dd2bb128
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-03 10:31:36 -0500

    add H5 component

---
+++ components/H5/H5.svelte
@@ -0,0 +1,23 @@
+<script>
+  // imports
+  import { twMerge } from 'tailwind-merge';
+  import { theme } from '../../stores';
+
+  // handlers
+
+  // utilities
+
+  // props (internal)
+  const defaultClasses = '';
+
+  // props (external)
+
+  // props (dynamic)
+  $: classes = twMerge(defaultClasses, $theme.h5, $$props.class);
+</script>
+
+<h5
+  class={classes}
+>
+  <slot />
+</h5>
--- components/index.js
+++ components/index.js
@@ -6,5 +6,6 @@ export { default as H1 } from "./H1/index.js";
 export { default as H2 } from "./H2/index.js";
 export { default as H3 } from "./H3/index.js";
 export { default as H4 } from "./H4/index.js";
+export { default as H5 } from "./H5/index.js";
 
 export { default as Legend } from "./Legend/index.js";
