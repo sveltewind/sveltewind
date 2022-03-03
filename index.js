d5f6ea504db5fa9fa95e9d10b80090b66d4b0fbd
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-03 09:24:47 -0500

    add index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import * as actions from './actions';
+import * as components from './components';
+import * as stores from './stores';
+import * as themes from './themes';
+
+export default { actions, components, stores, themes };
+export { actions, components, stores, themes };
