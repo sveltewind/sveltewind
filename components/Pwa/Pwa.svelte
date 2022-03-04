4f5417991ede73224e7416cc3c5cae6df981f0fa
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-04 09:55:21 -0500

    add Pwa component

---
+++ components/Pwa/Pwa.svelte
@@ -0,0 +1,27 @@
+<script>
+  // imports
+
+  // handlers
+
+  // utilities
+
+  // props (internal)
+
+  // props (external)
+
+  // props (dynamic)
+</script>
+
+<svelte:head>
+  <slot>
+    <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+    <link rel="alternate icon" href="/icon-16x16.png" />
+    <link rel="apple-touch-icon" href="/icon-apple-touch.png" />
+    <link rel="manifest" href="/manifest.json" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover" />
+    <meta name="theme-color" content="#FFF" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <!-- <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" /> -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+  </slot>
+</svelte:head>
---
+++ components/Pwa/index.js
@@ -0,0 +1,1 @@
+export { default } from "./Pwa.svelte";
