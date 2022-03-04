ec75081046f8950b0ee4dee542711aa9cbec6a57
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-04 09:55:44 -0500

    refactor for default slot

--- components/Modal/Modal.svelte
+++ components/Modal/Modal.svelte
@@ -13,12 +13,12 @@
   const defaultClasses = '';
 
   // props (external)
-  export let body;
-  export let showBodyClose = false;
+  export let body = 'Here is where the modal\'s body is added.  Lorem ipsum dollar sit anet.';
+  export let showBodyClose = true;
   export let showOverlay = true;
-  export let showTitleClose = false;
+  export let showTitleClose = true;
   export let show = false;
-  export let title;
+  export let title = 'Modal Title';
 
   // props (dynamic)
   $: classes = twMerge(defaultClasses, show ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none', $theme.modal, $$props.class);
@@ -30,30 +30,33 @@
   {#if showOverlay}
     <Overlay on:click={() => show = !show} class="cursor-pointer" />
   {/if}
-  {#if showBodyClose !== false || showTitleClose !== false || title !== undefined}
-    <Card class="relative space-y-[2rem] max-w-lg max-h-screen transform transition duration-500 {show ? 'scale-[1]' : 'scale-[.95]'}">
-      {#if showTitleClose !== false || title !== undefined}
-        <div class="flex items-center {showTitleClose === true && title !== undefined ? 'justify-between space-x-[2rem]' : showTitleClose === true ? 'justify-end' : 'justify-start'}">
-          {#if title !== undefined}
-            <H6>{title}</H6>
+  <slot>
+    {#if showBodyClose !== false || showTitleClose !== false || title !== undefined}
+      <div class="p-[1.5rem]">
+        <Card class="relative space-y-[2rem] max-w-lg max-h-screen transform transition duration-500 {show ? 'scale-[1]' : 'scale-[.95]'}">
+          {#if showTitleClose !== false || title !== undefined}
+            <div class="flex items-center {showTitleClose === true && title !== undefined ? 'justify-between space-x-[2rem]' : showTitleClose === true ? 'justify-end' : 'justify-start'}">
+              {#if title !== undefined}
+                <H6>{title}</H6>
+              {/if}
+              {#if showTitleClose !== false}
+              <Button
+                class="bg-transparent hover:bg-black/5 focus:bg-black/5 focus:ring-black/30 text-black px-[.5rem]"
+                on:click={() => (show = !show)}><Icon src={X} /></Button
+              >
+              {/if}
+            </div>
           {/if}
-          {#if showTitleClose !== false}
-          <Button
-            class="bg-transparent hover:bg-black/5 focus:bg-black/5 focus:ring-black/30 text-black px-[.5rem]"
-            on:click={() => (show = !show)}><Icon src={X} /></Button
-          >
+          {#if body !== undefined}
+            <P class="overflow-auto">{body}</P>
           {/if}
-        </div>
-      {/if}
-      {#if body !== undefined}
-        <P class="overflow-auto">{body}</P>
-      {/if}
-      {#if showBodyClose !== false}
-        <div class="flex justify-end">
-          <Button on:click={() => (show = !show)}>Close</Button>
-        </div>
-      {/if}
-    </Card>
-  {/if}
-  <slot />
+          {#if showBodyClose !== false}
+            <div class="flex justify-end">
+              <Button on:click={() => (show = !show)}>Close</Button>
+            </div>
+          {/if}
+        </Card>
+      </div>
+    {/if}
+  </slot>
 </div>
