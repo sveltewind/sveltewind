6789e4dce13ec144a9050c40d0ec54d138ea1f5f
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-15 10:14:06 -0400

    [ADD] auth store

--- stores/index.js
+++ stores/index.js
@@ -1,3 +1,4 @@
+export { default as auth } from './auth/index.js';
 export { default as pageLoaded } from './pageLoaded/index.js';
 export { default as socket } from './socket/index.js';
 export { default as theme } from './theme/index.js';
