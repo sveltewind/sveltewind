8dfee8c22c69a21c8fbc47de661f856f539e7d3f
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-20 08:43:47 -0400

    [ADD] mongodb key

--- stores/pageLoaded/index.js
+++ stores/pageLoaded/index.js
@@ -3,8 +3,9 @@ import { writable } from 'svelte/store';
 
 // items to
 const items = {
-	__layout: false,
-	socket: false
+  __layout: false,
+  mongodb: false,
+  socket: false,
 };
 
 export default writable(items);
