2cd659ed96adde1a99211021724fcbc0ed499907
Bob McAleavey <bobmcaleavey@gmail.com>
2022-03-20 08:44:34 -0400

    [REFACTOR] change store to custom store with defined methods

--- stores/socket/index.js
+++ stores/socket/index.js
@@ -1,3 +1,22 @@
 import { writable } from 'svelte/store';
 
-export default writable(undefined);
+const createStore = () => {
+  // destructure store
+  const { set, subscribe, update } = writable(undefined);
+
+  // methods
+  const emit = (event, data = {}, cb = () => {}) => {
+    subscribe(socket => {
+      socket.emit(event, data, cb);
+    });
+  };
+  const init = v => set(v);
+
+  return {
+    emit,
+    init,
+    subscribe,
+  };
+};
+
+export default createStore();
