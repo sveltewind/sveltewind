1d1fa4d8813200f29a9ea44a2ec0594375602c01
Bob McAleavey <bobmcaleavey@gmail.com>
2023-10-12 08:28:39 -0400

    feat: add copy action

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,5 @@
 export { default as clickOutside } from './clickOutside.js';
+export { copy } from 'svelte-copy';
 export { default as getEvents } from './getEvents.js';
 export { default as imask } from './imask.js';
 export { default as setType } from './setType.js';
