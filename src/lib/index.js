aede0a3cca236100524ef49ab3911050059b5082
Bob McAleavey <bobmcaleavey@gmail.com>
2023-11-16 13:56:44 -0500

    fix:fix typescript links

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,7 @@
-import * as actions from './actions/index.ts';
-import * as components from './components/index.ts';
-import * as stores from './stores/index.ts';
-import * as themes from './themes/index.ts';
+import * as actions from './actions/index.js';
+import * as components from './components/index.js';
+import * as stores from './stores/index.js';
+import * as themes from './themes/index.js';
 
 export default { actions, components, stores, themes };
 export { actions, components, stores, themes };
