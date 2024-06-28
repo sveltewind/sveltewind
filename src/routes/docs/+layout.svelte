44ce945587dbc4ca4a7382d3bca3035e9b00a4e9
Bob McAleavey <bobmcaleavey@gmail.com>
2024-06-28 10:27:36 -0400

    feat: convert doc menu to $state

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -1,72 +1,36 @@
 <script lang="ts">
   import { page } from '$app/stores';
-  import * as actions from '$lib/actions/index.js';
-  import * as components from '$lib/components/index.js';
-  import { A, Button, Card, Icon } from '$lib/components/index.js';
+  import { A, Button, Card, Drawer, Icon } from '$components/index.js';
   import { ChevronDown } from '$lib/icons/index.js';
-  import * as themes from '$lib/themes/index.js';
   import { slide } from 'svelte/transition';
   import { twMerge } from 'tailwind-merge';
+  import { onMount } from 'svelte';
+  import { menu } from './menu.svelte.js';
 
   // props
-  const sections = new Map([
-    [
-      'Getting Started',
-      { isOpen: false, items: new Map([['Installation', '/docs/installation']]) }
-    ],
-    [
-      'Actions',
-      {
-        isOpen: false,
-        items: Object.keys(actions).reduce((map, key) => {
-          const label = key
-            .replace(/(^.)/g, (character) => character.toUpperCase())
-            .replace(/([A-Z])/g, ' $1');
-          map.set(label, `/docs/actions/${key.toLowerCase()}`);
-          return map;
-        }, new Map())
-      }
-    ],
-    [
-      'Components',
-      {
-        isOpen: false,
-        items: Object.keys(components).reduce((map, key) => {
-          const label = key
-            .replace(/(^.)/g, (character) => character.toUpperCase())
-            .replace(/([A-Z])/g, ' $1');
-          map.set(label, `/docs/components/${key.toLowerCase()}`);
-          return map;
-        }, new Map())
-      }
-    ],
-    [
-      'Themes',
-      {
-        isOpen: false,
-        items: Object.keys(themes).reduce((map, key) => {
-          const label = key
-            .replace(/(^.)/g, (character) => character.toUpperCase())
-            .replace(/([A-Z])/g, ' $1');
-          map.set(label, `/docs/themes/${key.toLowerCase()}`);
-          return map;
-        }, new Map())
-      }
-    ]
-  ]);
+  let toggle: () => boolean;
+
+  onMount(() => {
+    menu.forEach(({ isOpen, items }, i) => {
+      if ([...items].map(([_, { href }]) => href).includes($page.url.pathname))
+        menu[i].isOpen = true;
+    });
+  });
 </script>
 
 <div class="flex flex-grow overflow-auto">
   <Card
-    class="overflow-auto rounded-none bg-transparent dark:bg-transparent min-w-[15rem] relative divide-y divide-slate-200 dark:divide-slate-800"
+    class="hidden lg:flex overflow-auto rounded-none bg-transparent dark:bg-transparent min-w-[15rem] relative divide-y divide-slate-200 dark:divide-slate-800"
   >
-    {#each [...sections] as [sectionTitle, { isOpen, items: sectionItems }]}
+    {#each menu as { title, isOpen, items }, i}
       <div class="flex flex-col py-2 first:pt-0">
         <Button
-          class="font-bold text-slate-950 dark:text-slate-50 py-2 bg-transparent hover:bg-transparent focus:bg-transparent dark:shadow-none justify-between px-0 text-base font-medium"
-          onclick={() => (isOpen = !isOpen)}
+          class="font-bold text-slate-950 dark:text-slate-50 py-2 bg-transparent hover:bg-transparent focus:bg-transparent dark:shadow-none justify-between px-0 text-base"
+          onclick={() => {
+            menu[i].isOpen = !menu[i].isOpen;
+          }}
         >
-          <span>{sectionTitle}</span>
+          <span>{title}</span>
           <Icon
             class={twMerge('h-4 w-4 transition duration-200', isOpen ? 'rotate-0' : '-rotate-90')}
             src={ChevronDown}
@@ -74,10 +38,10 @@
         </Button>
         {#if isOpen}
           <div class="flex flex-col" transition:slide={{ duration: 200 }}>
-            {#each [...sectionItems] as [label, href]}
+            {#each [...items] as [label, { href }]}
               <A
                 class={twMerge(
-                  'hover:border-slate-950 focus:border-slate-950 dark:hover:border-slate-50 dark:focus:border-slate-50 border-l border-slate-200 dark:border-slate-800 px-4 py-2 shadow-none dark:shadow-none hover:text-slate-950 focus:text-slate-950 dark:hover:text-slate-50 dark:focus:text-slate-50',
+                  'hover:shadow-none hover:border-slate-950 focus:border-slate-950 dark:hover:border-slate-50 dark:focus:border-slate-50 border-l border-slate-200 dark:border-slate-800 px-4 py-2 shadow-none dark:shadow-none hover:text-slate-950 focus:text-slate-950 dark:hover:text-slate-50 dark:focus:text-slate-50',
                   $page.url.pathname === href
                     ? 'border-violet-500 dark:border-violet-500 text-slate-950 dark:text-slate-50 hover:border-violet-500 focus:border-violet-500 dark:hover:border-violet-500 dark:focus:border-violet-500'
                     : undefined
@@ -92,7 +56,43 @@
       </div>
     {/each}
   </Card>
-  <div>
+  <div class="flex flex-col flex-grow overflow-auto p-4 space-y-8">
+    <!-- <Button
+      class="text-current lg:hidden shadow-none self-start px-3 bg-transparent hover:bg-slate-950/10 focus:bg-slate-950/10 focus:ring-slate-950/30 dark:hover:bg-slate-50/5 dark:focus:bg-slate-50/5 dark:focus:ring-slate-50/30"
+      onclick={toggle}
+    >
+      <Icon src={Bars2} />
+    </Button> -->
     <slot />
   </div>
 </div>
+
+<Drawer bind:toggle class="w-full max-w-[calc(100vw_-_2rem)]">
+  <Card class="flex-grow rounded-none overflow-auto text-lg">
+    {#each menu as { title, items }, i}
+      <div class="flex flex-col py-2 first:pt-0">
+        <div
+          class="font-bold text-slate-950 dark:text-slate-50 py-2 bg-transparent hover:bg-transparent focus:bg-transparent dark:shadow-none justify-between px-0 text-lg"
+        >
+          <span>{title}</span>
+        </div>
+        <div class="flex flex-col" transition:slide={{ duration: 200 }}>
+          {#each [...items] as [label, href]}
+            <A
+              class={twMerge(
+                'hover:shadow-none hover:border-slate-950 focus:border-slate-950 dark:hover:border-slate-50 dark:focus:border-slate-50 border-l border-slate-200 dark:border-slate-800 px-4 py-2 shadow-none dark:shadow-none hover:text-slate-950 focus:text-slate-950 dark:hover:text-slate-50 dark:focus:text-slate-50',
+                $page.url.pathname === href
+                  ? 'border-violet-500 dark:border-violet-500 text-slate-950 dark:text-slate-50 hover:border-violet-500 focus:border-violet-500 dark:hover:border-violet-500 dark:focus:border-violet-500'
+                  : undefined
+              )}
+              {href}
+              onclick={toggle}
+            >
+              {label}
+            </A>
+          {/each}
+        </div>
+      </div>
+    {/each}
+  </Card>
+</Drawer>
---
+++ src/routes/docs/menu.svelte.ts
@@ -0,0 +1,54 @@
+import * as actions from '$lib/actions/index.js';
+import * as components from '$components/index.js';
+import * as themes from '$lib/themes/index.js';
+
+// utilities
+const getLabel = (key: string) =>
+  key.replace(/(^.)/g, (character) => character.toUpperCase()).replace(/([A-Z])/g, ' $1');
+const getHREF = (key: string) =>
+  key
+    .replace(/(^.)/g, (character) => character.toLowerCase())
+    .replace(/([A-Z])/g, '-$1')
+    .toLowerCase();
+
+export const menu: {
+  title: string;
+  isOpen: boolean;
+  items: Map<string, { href: string }>;
+}[] = $state([
+  {
+    title: 'Getting Started',
+    isOpen: false,
+    items: new Map([
+      ['Installation', { href: '/docs/getting-started/installation' }],
+      ['Using Components', { href: '/docs/getting-started/using-components' }]
+    ])
+  },
+  {
+    title: 'Actions',
+    isOpen: false,
+    items: Object.keys(actions).reduce((map, key) => {
+      const label = getLabel(key);
+      map.set(label, { href: `/docs/actions/${getHREF(key)}` });
+      return map;
+    }, new Map())
+  },
+  {
+    title: 'Components',
+    isOpen: false,
+    items: Object.keys(components).reduce((map, key) => {
+      const label = getLabel(key);
+      map.set(label, { href: `/docs/components/${getHREF(key)}` });
+      return map;
+    }, new Map())
+  },
+  {
+    title: 'Themes',
+    isOpen: false,
+    items: Object.keys(themes).reduce((map, key) => {
+      const label = getLabel(key);
+      map.set(label, { href: `/docs/themes/${getHREF(key)}` });
+      return map;
+    }, new Map())
+  }
+]);
