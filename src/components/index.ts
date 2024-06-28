5ef2c8f2715c15fc8891c0f259c6c9f2987a1b18
Bob McAleavey <bobmcaleavey@gmail.com>
2024-06-28 14:04:42 -0400

    feat: add Logo component to frontend

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from '$lib/components/index.js';
 export { default as Code } from './Code.svelte';
 export { default as CopyButton } from './CopyButton.svelte';
+export { default as Logo } from './Logo.svelte';
 export { default as SyntaxHighlighter } from './SyntaxHighlighter.svelte';
