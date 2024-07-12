e7bb5bf8d3d589a64b3d0e4863c515cfb14f9253
Bob McAleavey <bobmcaleavey@gmail.com>
2024-07-12 09:32:27 -0400

    feat: add appStatusBar & dynamicIsland snippets

--- src/components/IphoneMockup.svelte
+++ src/components/IphoneMockup.svelte
@@ -4,7 +4,7 @@
 	import { theme } from '$lib/index.js';
 	import { BatteryFull, Send, Signal, Wifi } from 'lucide-svelte';
 	import { DateTime } from 'luxon';
-	import { onMount } from 'svelte';
+	import { onMount, type Snippet } from 'svelte';
 	import { twMerge } from 'tailwind-merge';
 
 	// handlers
@@ -17,9 +17,12 @@
 	let classes = $state('');
 	let date = $state(DateTime.now());
 	let {
+		appStatusBar,
 		appStatusBarIsVisible = $bindable(),
 		class: className = undefined,
 		children,
+		dynamicIsland,
+		dynamicIslandIsVisible = $bindable(),
 		isVisible = $bindable(),
 		orientation = $bindable(),
 		this: elem = $bindable(),
@@ -27,9 +30,12 @@
 		use = [],
 		...props
 	}: {
+		appStatusBar?: Snippet;
 		appStatusBarIsVisible?: boolean;
 		class?: string;
 		children?: any;
+		dynamicIsland?: Snippet;
+		dynamicIslandIsVisible?: boolean;
 		isVisible?: boolean;
 		orientation?: 'horizontal' | 'vertical';
 		this?: any;
@@ -42,9 +48,12 @@
 		if (appStatusBarIsVisible === undefined) appStatusBarIsVisible = true;
 	});
 	$effect(() => {
+		if (dynamicIslandIsVisible === undefined) dynamicIslandIsVisible = true;
+	});
+	$effect(() => {
 		if (orientation === undefined) orientation = 'vertical';
 		classes = twMerge(
-			'relative overflow-hidden rounded-xl h-[600px] p-0 shadow-none border-[4px] border-[#0E0E0E]',
+			'relative overflow-hidden rounded-xl h-[600px] p-0 shadow-none border ring-offset-[8px] ring-offset-[#0E0E0E] ring-[1px] ring-white/5 dark:ring-white/5 border-white/5',
 			orientation === 'vertical' ? 'aspect-[9/19.5]' : 'aspect-[19.5/9]',
 			className
 		);
@@ -64,17 +73,32 @@
 			{@render children()}
 		{/if}
 		{#if appStatusBarIsVisible}
-			<Header class="absolute left-0 top-0 flex w-full justify-between p-4 ring-0">
-				<Div class="flex items-center space-x-1 text-xs">
-					<Div>{date.toFormat('h:mm')}</Div>
-					<Send class="h-[.75rem] w-[.75rem]" />
+			{#if appStatusBar}
+				{@render appStatusBar()}
+			{:else}
+				<Header class="absolute left-0 top-0 flex w-full justify-between p-4 ring-0">
+					<Div class="flex items-center space-x-1 text-xs">
+						<Div>{date.toFormat('h:mm')}</Div>
+						<Send class="h-[.75rem] w-[.75rem]" />
+					</Div>
+					<Div class="flex items-center space-x-1">
+						<Signal class="h-[.75rem] w-[.75rem]" />
+						<Wifi class="h-[.75rem] w-[.75rem]" />
+						<BatteryFull class="h-[.75rem] w-[.75rem] text-green-500" />
+					</Div>
+				</Header>
+			{/if}
+		{/if}
+		{#if dynamicIslandIsVisible}
+			{#if dynamicIsland}
+				{@render dynamicIsland()}
+			{:else}
+				<Div
+					class="absolute left-1/2 top-2 flex aspect-[4/1] h-6 -translate-x-1/2 justify-end rounded-full bg-black p-1"
+				>
+					<Div class="aspect-square h-full rounded-full bg-[#10132E]" />
 				</Div>
-				<Div class="flex items-center space-x-1">
-					<Signal class="h-[.75rem] w-[.75rem]" />
-					<Wifi class="h-[.75rem] w-[.75rem]" />
-					<BatteryFull class="h-[.75rem] w-[.75rem] text-green-500" />
-				</Div>
-			</Header>
+			{/if}
 		{/if}
 	</Card>
 {/if}
