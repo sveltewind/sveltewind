4e7d39511d1f40435a99b38dca3a510004e09531
Bob McAleavey <bobmcaleavey@gmail.com>
2026-05-25 16:59:39 -0400

    feat: add additional exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,6 @@
 // Reexport your entry components here
+export * as attachments from './attachments/index.js';
 export * as components from './components/index.js';
+export * as icons from './icons/index.js';
 export { theme } from './theme/index.js';
+export * as themes from './themes/index.js';
